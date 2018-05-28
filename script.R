e3a992515401b984860c810069b37e6ccc0fc955
JohMueller <johannes.m@correlaid.org>
2018-05-28 16:52:23 +0200

    update

--- script.R
+++ script.R
@@ -1,3 +1,5 @@
+# install.packages(c("readr", "caret", "ggplot2")
+
 library(readr)
 library(caret)
 library(ggplot2)
@@ -7,8 +9,8 @@ data <- read_csv("~/projects/blood_donation/data.csv")
 names(data) <- c("ID", "months_since_last", "number_donations",
                  "total_volume", "months_since_first", "made_donation")
 data[2:5] <- sapply(data[2:5], as.numeric)
-data$ID <- NULL
 data$made_donation <- as.factor(data$made_donation)
+data$ID <- NULL
 
 # split into training and test data
 Index <- createDataPartition(data$made_donation, p=0.80, list=FALSE)
@@ -22,7 +24,6 @@ ncol(data_train)
 summary(data_train)
 
 plot(density(data_train$total_volume))
-cor(data_train[,2:5])
 
 ggplot(data_train, aes(x=total_volume, 
                        y=number_donations))+
@@ -33,10 +34,10 @@ ggplot(data_train, aes(x=total_volume,
 fit.glm <- train(made_donation ~ months_since_last + number_donations + months_since_first,
                  data=data_train, method="glm", metric="Accuracy")
 
-summary(resamples(fit.glm))
+summary(fit.glm)
 predict(fit.glm)
-sum(predict(fit.glm) == data_train$made_donation)/nrow(data_train)
 
-# check accurcy on test data
+# check accuracy on test data
+predict(fit.glm, newdata = data_test)
 sum(predict(fit.glm, newdata = data_test) == data_test$made_donation)/nrow(data_test)
 
